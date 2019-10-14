7369a0c7ca322d9f0b81547745be483ac578b38a
tsteinberg28 <54481780+tsteinberg28@users.noreply.github.com>
2019-10-14 09:12:11 -0400

    Update from Tom and minor changes

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -8,6 +8,9 @@ var waitingApp = new Vue({
       fetch('api/waiting/')
       .then(response => response.json())
       .then(json => { waitingApp.patients = json })
+    },
+    formatLocalVisitDate(d) {
+      return moment.utc(d).local().format("HH:mm MMM Do");
     }
   },
   created() {
